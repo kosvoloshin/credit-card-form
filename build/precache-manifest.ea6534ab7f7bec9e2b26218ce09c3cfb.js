252155638a98e5214ec432236b5a2aaafe072547
Konstantin Voloshyn <23.03@i.ua>
2020-03-22 15:37:14 +0200

    added build

---
+++ build/precache-manifest.ea6534ab7f7bec9e2b26218ce09c3cfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5df7168f8d53e51ff889dc1eb8d851",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e9e2b71568ffd64d1c2",
+    "url": "/static/js/2.e515c15d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e515c15d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "766ff49119596d479bd9",
+    "url": "/static/js/main.c6b56f3f.chunk.js"
+  },
+  {
+    "revision": "308f008002e88317b92a",
+    "url": "/static/js/runtime-main.bba7c8fa.js"
+  }
+]);
